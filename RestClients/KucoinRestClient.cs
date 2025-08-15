d165acd9b48b0b2f389d2e085a6c8516b2f07b08
senyich <arseniyneshetin@gmail.com>
2025-08-15 13:12:02 +0700

    add kucoin

--- RestClients/KucoinRestClient.cs
+++ RestClients/KucoinRestClient.cs
@@ -1,22 +1,27 @@
 using System.Globalization;
 using System.Net.Http.Json;
+using System.Security.Cryptography;
+using System.Text;
 using CryptoExchangesRestLibrary.Models;
 using CryptoExchangesRestLibrary.RestClients.Abstraction;
 using CryptoExchangesRestLibrary.SerializationClasses;
+using Newtonsoft.Json;
+using JsonException = System.Text.Json.JsonException;
 
 namespace CryptoExchangesRestLibrary.RestClients;
 
 public class KucoinRestClient : ExchangeRestClient
 {
     private const string _host = "https://api.kucoin.com";
-
+    private string _passPhrase = null;
     public KucoinRestClient()
         : base()
     { }
 
     public void SetApiCredentials(string apiKey, string apiSecret)
         => _apiCredentials = new ApiCredendetails(apiKey, apiSecret);
-
+    public void SetPassPhrase(string passPhrase)
+        => _passPhrase = passPhrase;
     public override async Task<OrderbookResponce> GetOrderbookAsync(string symbol, int limit = 20)
     {
         symbol = symbol.Contains("USDT") 
@@ -30,7 +35,8 @@ public class KucoinRestClient : ExchangeRestClient
         if (!response.IsSuccessStatusCode)
             throw new HttpRequestException($"[KucoinExchange]:  ошибка при получении ордербука по паре {symbol}");
         var result = await response.Content.ReadFromJsonAsync<KucoinOrderbookResponse>();
-        
+        if (result == null || result.Data == null )
+            throw new JsonException("[KucoinExchange]: ошибка при десериализации ответа");
         var asks = result.Data.Asks
             .ToDictionary(
                 x => decimal.Parse(x[0], CultureInfo.InvariantCulture), 
@@ -51,20 +57,58 @@ public class KucoinRestClient : ExchangeRestClient
     {
         throw new NotImplementedException();
     }
-
     public override async Task<WithdrawalDataResponce> GetWithdrawalDataAsync(string symbol)
     {
-        symbol = symbol.Contains("USDT") ? symbol.Replace("USDT", string.Empty) : symbol;
-        string path = $"/api/v1/withdrawals/quotas?currency={symbol}&chain";
-        Uri uri = new Uri($"{_host}{path}");
-        var response = await _client.GetAsync(uri);
-        // if (!response.IsSuccessStatusCode)
-        //     throw new HttpRequestException("[KucoinExchange]: ошибка при получении информации для перевода");
-        var tempresponce = await response.Content.ReadAsStringAsync();
-        Console.WriteLine(tempresponce);
-        return null;
-    }
+        symbol = symbol.Replace("USDT", "", StringComparison.OrdinalIgnoreCase).ToUpper();
+        string path = $"/api/v1/withdrawals/quotas?currency={symbol}";
+    
+        long timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+    
+        string method = "GET";
+        string endpoint = path;
+        string strToSign = $"{timestamp}{method}{endpoint}";
 
+        string signature = GenerateSignature(strToSign, _apiCredentials.apiSecret);
+    
+        string signedPassphrase = GenerateSignature(_passPhrase, _apiCredentials.apiSecret);
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{_host}{path}");
+        request.Headers.Add("KC-API-KEY", _apiCredentials.apiKey);
+        request.Headers.Add("KC-API-SIGN", signature);
+        request.Headers.Add("KC-API-TIMESTAMP", timestamp.ToString());
+        request.Headers.Add("KC-API-PASSPHRASE", signedPassphrase); 
+        request.Headers.Add("KC-API-KEY-VERSION", "2"); 
+
+        using var response = await _client.SendAsync(request);
+    
+        if (!response.IsSuccessStatusCode)
+        {
+            string errorContent = await response.Content.ReadAsStringAsync();
+            throw new HttpRequestException($"[KucoinClient]: ошибка получения информации о переводе: {response.StatusCode}, {errorContent}");
+        }
+        var tempResponse = await response.Content.ReadFromJsonAsync<KucoinWithdrawalLimitResponse>();
+        if (tempResponse == null)
+            throw new JsonException("[KucoinClient]: ошибка десериализации ответа");
+        var withdrawalData = new WithdrawalDataResponce(
+            Symbol: tempResponse.Data.Currency,
+            Chains: new List<BlockchainDataResponce>
+            {
+                new BlockchainDataResponce(
+                    Name: tempResponse.Data.Chain,
+                    FullName: tempResponse.Data.Chain, 
+                    CanWithdraw: tempResponse.Data.IsWithdrawEnabled,
+                    CanDeposit: true,
+                    Fee: decimal.Parse(tempResponse.Data.WithdrawMinFee, CultureInfo.InvariantCulture))
+            }
+        );
+        return withdrawalData;
+    }
+    private string GenerateSignature(string data, string apiSecret)
+    {
+        using var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(apiSecret));
+        byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+        return Convert.ToBase64String(hash);
+    }
     public override string GetUrl(string symbol)
     {
         throw new NotImplementedException();
--- SerializationClasses/KucoinRecords.cs
+++ SerializationClasses/KucoinRecords.cs
@@ -13,3 +13,26 @@ public record class KucoinOrderbookResponse(
     string Code,
     KucoinOrderbookData Data
 );
+public record class KucoinWithdrawalLimitData(
+    string Currency,
+    [property: JsonPropertyName("limitBTCAmount")] string LimitBTCAmount,
+    [property: JsonPropertyName("usedBTCAmount")] string UsedBTCAmount,
+    string QuotaCurrency,
+    [property: JsonPropertyName("limitQuotaCurrencyAmount")] string LimitQuotaCurrencyAmount,
+    [property: JsonPropertyName("usedQuotaCurrencyAmount")] string UsedQuotaCurrencyAmount,
+    [property: JsonPropertyName("remainAmount")] string RemainAmount,
+    [property: JsonPropertyName("availableAmount")] string AvailableAmount,
+    [property: JsonPropertyName("withdrawMinFee")] string WithdrawMinFee,
+    [property: JsonPropertyName("innerWithdrawMinFee")] string InnerWithdrawMinFee,
+    [property: JsonPropertyName("withdrawMinSize")] string WithdrawMinSize,
+    [property: JsonPropertyName("isWithdrawEnabled")] bool IsWithdrawEnabled,
+    int Precision,
+    string Chain,
+    string? Reason,
+    [property: JsonPropertyName("lockedAmount")] string LockedAmount
+);
+
+public record class KucoinWithdrawalLimitResponse(
+    string Code,
+    KucoinWithdrawalLimitData Data
+);
